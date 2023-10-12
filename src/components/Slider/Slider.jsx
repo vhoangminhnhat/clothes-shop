0f130d22817d42a7c70cac0f2c900acb8fb14099
bnm55vnn <90960555+vhoangminhnhat@users.noreply.github.com>
2023-10-12 23:25:42 +0700

    CSS, function and UI for Carousel ( dummy data )

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Slider.css";
 import { EastOutlined, WestOutlined } from '@mui/icons-material';
 
+const data = [
+  "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+];
+
 const SliderComponent = () => {
-    const data = [
-        "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    ]
+  const [carousel, setCarousel] = useState(0);
+
+  const previousCarousel = () => {
+    setCarousel(carousel === 0 ? 2 : (value) => value - 1);
+  };
+
+  const nextCarousel = () => {
+    setCarousel(carousel === 2 ? 0 : (value) => value + 1);
+  };
+
   return (
     <div className='slider'>
-        <div className='container'>
+        <div className='container' style={{transform: `translateX(-${carousel*100}vw)`}}>
             <img src={data[0]} alt='carousel'/>
             <img src={data[1]} alt='carousel'/>
             <img src={data[2]} alt='carousel'/>
         </div>
        <div className='icons'>
-        <div className='icon'>
+        <div className='icon' onClick={previousCarousel}>
           <WestOutlined/>
         </div>
-        <div className='icon'>
+        <div className='icon'onClick={nextCarousel}>
           <EastOutlined/>
         </div>
        </div>
